b999cba3d14ebc431725e10d407484465969e95e
Jacob Yarborough <jayarborough95@gmail.com>
2024-07-19 09:33:04 -0500

    Add generator routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "welcome#index"
+
+  namespace :api do
+    resources :generator, only: [:create]
+  end
 end
